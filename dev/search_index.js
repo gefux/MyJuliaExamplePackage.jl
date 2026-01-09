2fb772764771bb97843a889a449df39756df0736
Documenter.jl <documenter@juliadocs.github.io>
2026-01-09 17:13:23 +0000

    build based on fa142f4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#MyJuliaExamplePackage","page":"Home","title":"MyJuliaExamplePackage","text":"Documentation for MyJuliaExamplePackage.\n\n","category":"section"}]
+[{"location":"#MyJuliaExamplePackage","page":"Home","title":"MyJuliaExamplePackage","text":"Documentation for MyJuliaExamplePackage.\n\n","category":"section"},{"location":"#The-API-(in-full-glory)","page":"Home","title":"The API (in full glory)","text":"","category":"section"},{"location":"#MyJuliaExamplePackage.foofunc-Tuple{Any, Any}","page":"Home","title":"MyJuliaExamplePackage.foofunc","text":"foofunc(x,y)\n\nTake that x and add the double of that y.\n\n\n\n\n\n","category":"method"}]
 }
